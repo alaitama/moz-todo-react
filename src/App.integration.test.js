813a57f786b3e7bb6e5a623e58cb1228c166e062
Alberto Laita <alberto.laita@ocado.com>
2023-10-13 23:08:03 +0200

    Added test

--- src/App.integration.test.js
+++ src/App.integration.test.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 const DATA = [
     { id: "todo-0", name: "Eat", completed: true },
     { id: "todo-1", name: "Sleep", completed: false },
-    { id: "todo-2", name: "Repeat", completed: false },
+    { id: "todo-2", name: "Click-me", completed: false },
   ];
 
 it('renders welcome message', () => {
   render(<App tasks={DATA} />);
   expect(screen.getByText('TodoMatic')).toBeInTheDocument();
+});
+
+
+
+it('tick checkbox when click', () => {
+  render(<App tasks={DATA} />);
+  const checkbox = screen.getByRole("checkbox", { name: "Click-me" });
+  expect(checkbox).not.toBeChecked();
+
+  fireEvent.click(checkbox);
+
+  expect(checkbox).toBeChecked();
 });
